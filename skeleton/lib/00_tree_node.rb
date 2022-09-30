ad8207387ce38e343955b381007f80e876d23741
“sajeremy” <9jsantiago4@gmail.com>
2022-09-30 12:12:32 -0400

    first go

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,17 @@
+class PolyTreeNode
+    
+    attr_reader :value, :parent, :children
+    
+    def initialize(value,parent = nil,children = [])
+        @value = value
+        @parent = parent
+        @children = children
+    end
+
+    def parent=
+
+    end
+
+
+
+end
